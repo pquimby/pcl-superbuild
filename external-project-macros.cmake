2fe9cdbfb8d6cc2828395caf392714e9bd7c167f
Joel Teply <joelteply@yahoo.com>
2016-01-13 17:02:03 -0600

    use official pcl build

--- external-project-macros.cmake
+++ external-project-macros.cmake
@@ -183,7 +183,7 @@ macro(fetch_pcl)
   ExternalProject_Add(
     pcl-fetch
     SOURCE_DIR ${source_prefix}/pcl
-    GIT_REPOSITORY git://github.com/patmarion/PCL.git
+    GIT_REPOSITORY git://github.com/PointCloudLibrary/pcl.git
     GIT_TAG origin/android-tag
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
